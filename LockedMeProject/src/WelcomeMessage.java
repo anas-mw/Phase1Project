9ddadacc0bc7e5a8e326e49042428ccf820a7e45
Anas <109147211+anas-mw@users.noreply.github.com>
2022-07-19 13:12:57 +0100

    Delete WelcomeMessage.java

--- LockedMeProject/src/WelcomeMessage.java
+++
@@ -1,9 +0,0 @@
-class Message {
-	public void display() {
-		System.out.println("Welcome to the LockedMe prototype Application");
-		System.out.println("Application Name: LockedMe");
-		System.out.println("Developer Name: Anas Khan");
-		System.out.println("____________________________________________________");
-
-	}
-}
